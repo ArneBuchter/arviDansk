8888ce5430ad6c78591160a88d1a261f6c80a3c5
Arne Emil Buchter <5651612@RTS-365.dk>
2020-06-16 11:27:18 +0200

    removeSpace

--- src/App.js
+++ src/App.js
@@ -5,10 +5,8 @@ import Navbar from './components/Navbar/Navbar';
 import Hero from './components/Hero/Hero';
 import Contentbox from './components/Contentbox/Contentbox';
 import Services from './components/What-we-do/Services';
-import Space from './components/space/Space';
 import Support from './components/Support/Support';
 import Marketing from './components/Marketing/Marketing';
-import Spacetwo from './components/space/Spacetwo';
 import Footer from './components/Footer/Footer';
 
 function App() {
@@ -20,10 +18,8 @@ function App() {
            <Hero />
            <Contentbox />
            <Services />
-    {/*        <Space /> */}
            <Support />
            <Marketing />
-           <Spacetwo />
            <Footer />
         </section>
     </main>
--- src/components/space/Space.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import './space.modules.css';
-
-export default () => (
-    <div className="space"></div>
-)
