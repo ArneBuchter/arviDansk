b43aa5559c35ae74ec3d809fb5f9023b5fda9503
Arne Emil Buchter <5651612@RTS-365.dk>
2020-06-19 13:07:28 +0200

    addTarget

--- src/components/Navbar/Nav-link.js
+++ src/components/Navbar/Nav-link.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-export default function NavLink({ title, href }) {
+export default function NavLink({ title, href, target }) {
     return (
-        <li className="nav__item"><a href={href} className="nav__link">{title}</a></li>
+        <li className="nav__item"><a href={href} target={target} className="nav__link">{title}</a></li>
     )
 }
 
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,11 +4,11 @@ import NavLink from './Nav-link';
 
 export default () => (
     <nav className="nav">
-        <a href="https://arvi.dk" target="blank" className="nav__logo">arvi</a>
+        <a href="https://angry-banach-514b1a.netlify.app/" target="blank" className="nav__logo">arvi</a>
         <ul>
             <NavLink title="Prices" href="#prices" />
             <NavLink title="What we do" href="#services" />
-            <NavLink title="Portfolio" href="https://blissful-almeida-9daaba.netlify.app/" />
+            <NavLink title="Portfolio" href="https://blissful-almeida-9daaba.netlify.app/" target="blank" />
             <NavLink title="Support" href="#support" />
             <NavLink title="Marketing" href="#marketing" />
             <NavLink title="Contact" href="#contact" />
